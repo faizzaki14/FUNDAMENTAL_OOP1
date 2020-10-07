87cd4de4e3f020d1f357bf0199dbd01e4f9c3b3f
Faiz Zaki Ramadhan <56437913+faizzaki14@users.noreply.github.com>
2020-10-08 00:06:12 +0700

    Initial commit

---
+++ src/com/Faizzakiramadhan/Object/Objek.java
@@ -0,0 +1,26 @@
+package com.Faizzakiramadhan.Object;
+
+import javax.naming.Name;
+
+public class Objek {
+    public static void main(String[] args) {
+
+        //Deklarasi object Tanpa parameter
+        Mahasiswa mahasiswaTanpa = new Mahasiswa();
+
+        mahasiswaTanpa.setNama("Faiz Zaki Ramadhan");
+        mahasiswaTanpa.setNim(19104075);
+
+        System.out.println("Mahasiswa 1");
+        System.out.println("Nama\t : " + mahasiswaTanpa.getNama());
+        System.out.println("Nim\t\t : " + mahasiswaTanpa.getNim());
+
+        //Deklarasi object dengan parameter
+        Mahasiswa mahasiswa = new Mahasiswa("Faiz Zaki Ramadhan", 19104075);
+
+        System.out.println("Mahasiswa 2");
+        System.out.println("Nama\t : " + mahasiswaTanpa.getNama());
+        System.out.println("Nim\t\t : " + mahasiswaTanpa.getNim());
+
+    }
+}
